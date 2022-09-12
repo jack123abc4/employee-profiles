7c44016d511dd6090673986492c351a8b89e72b0
jack123abc4 <jack.r.motherway@gmail.com>
2022-09-12 14:51:40 -0400

    manager class

--- index.js
+++ index.js
@@ -8,5 +8,7 @@ const Manager = require("./lib/Manager.js");
 const inquirer = require("inquirer");
 
 e = new Employee("Jack",0,"jack@test.com");
+m = new Manager("John",1,"john@test.com",101);
 
-console.log(e.getName(),e.getId(),e.getEmail());
+console.log(e.getName(),e.getId(),e.getEmail(),e.getRole());
+console.log(m.getName(),m.getId(),m.getEmail(),m.getRole(),m.officeNumber);
