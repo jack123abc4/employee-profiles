5cd49595623b235d83d8be4f53802df77e46c7c8
jack123abc4 <jack.r.motherway@gmail.com>
2022-09-12 14:53:58 -0400

    engineer class

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const inquirer = require("inquirer");
 
 e = new Employee("Jack",0,"jack@test.com");
 m = new Manager("John",1,"john@test.com",101);
+eng = new Engineer("Jason", 2, "jason@test.com", "jasonGit");
 
 console.log(e.getName(),e.getId(),e.getEmail(),e.getRole());
 console.log(m.getName(),m.getId(),m.getEmail(),m.getRole(),m.officeNumber);
+console.log(eng.getName(),eng.getId(),eng.getEmail(),eng.getRole(),eng.getGithub());
