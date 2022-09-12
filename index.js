e70b551a2bcbd380877aff2a36c29b87181f9938
jack123abc4 <jack.r.motherway@gmail.com>
2022-09-12 14:35:55 -0400

    imports

---
+++ index.js
@@ -0,0 +1,10 @@
+// importing custom classes
+const Employee = require("./lib/Employee.js");
+const Engineer = require("./lib/Engineer.js");
+const Intern = require("./lib/Intern.js");
+const Manager = require("./lib/Manager.js");
+
+// importing inquirer
+const inquirer = require("inquirer");
+
+console.log("imported.");
