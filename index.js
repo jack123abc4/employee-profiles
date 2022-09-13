fd34de1d775b84f05531ad26202db796318cb04d
jack123abc4 <jack.r.motherway@gmail.com>
2022-09-13 15:58:53 -0400

    start of day push

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ const Manager = require("./lib/Manager.js");
 // importing inquirer
 const inquirer = require("inquirer");
 
-employee = new Employee("Jack",0,"jack@test.com");
-manager = new Manager("John",1,"john@test.com",101);
-engineer = new Engineer("Jason", 2, "jason@test.com", "jasonGit");
-intern = new Intern("Jacqueline", 3, "jackie@test.com","Harvard");
+// employee = new Employee("Jack",0,"jack@test.com");
+// manager = new Manager("John",1,"john@test.com",101);
+// engineer = new Engineer("Jason", 2, "jason@test.com", "jasonGit");
+// intern = new Intern("Jacqueline", 3, "jackie@test.com","Harvard");
 
-console.log(employee.getName(),employee.getId(),employee.getEmail(),employee.getRole());
-console.log(manager.getName(),manager.getId(),manager.getEmail(),manager.getRole(),manager.officeNumber);
-console.log(engineer.getName(),engineer.getId(),engineer.getEmail(),engineer.getRole(),engineer.getGithub());
-console.log(intern.getName(),intern.getId(),intern.getEmail(),intern.getRole(),intern.getSchool());
+// console.log(employee.getName(),employee.getId(),employee.getEmail(),employee.getRole());
+// console.log(manager.getName(),manager.getId(),manager.getEmail(),manager.getRole(),manager.officeNumber);
+// console.log(engineer.getName(),engineer.getId(),engineer.getEmail(),engineer.getRole(),engineer.getGithub());
+// console.log(intern.getName(),intern.getId(),intern.getEmail(),intern.getRole(),intern.getSchool());
+
