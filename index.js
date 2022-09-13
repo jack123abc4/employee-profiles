2202f939875028acd858f03b7233add153b8b274
jack123abc4 <jack.r.motherway@gmail.com>
2022-09-13 16:22:30 -0400

    manager prompt functionality

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const Manager = require("./lib/Manager.js");
 // importing inquirer
 const inquirer = require("inquirer");
 
+
+let employeeList = [];
 // employee = new Employee("Jack",0,"jack@test.com");
 // manager = new Manager("John",1,"john@test.com",101);
 // engineer = new Engineer("Jason", 2, "jason@test.com", "jasonGit");
@@ -17,3 +19,40 @@ const inquirer = require("inquirer");
 // console.log(engineer.getName(),engineer.getId(),engineer.getEmail(),engineer.getRole(),engineer.getGithub());
 // console.log(intern.getName(),intern.getId(),intern.getEmail(),intern.getRole(),intern.getSchool());
 
+function createManager() {
+    // name, email, officenumber
+    inquirer.prompt([
+    {
+        type: "input",
+        message: "What is the manager's name?",
+        name: "name",
+    },
+    {
+        type: "input",
+        message: "What is the manager's email?",
+        name: "email",
+    },
+    {
+        type: "input",
+        message: "What is the manager's office number?",
+        name: "officeNum",
+    },
+    ])
+    .then((response) => {
+        console.log(response);
+        employeeList.push(new Manager(response.name, employeeList.length, response.email, response.officeNum));
+        console.log("list:",employeeList);
+        createManager();
+    }
+    );
+}
+
+function createEngineer() {
+    // name, email, github
+}
+
+function createIntern() {
+    // name, email, school
+}
+
+createManager();
